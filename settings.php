c808e30902838411d42417bcc570096f7470ccb2
EldinSalkovic <eldinsalkoviic@gmail.com>
2015-04-16 13:43:39 +0200

    First commit

---
+++ settings.php
@@ -0,0 +1,19 @@
+<?php
+	$adresa[0]="111 Railroad Avenue Loveland OH 45140"; //jako je vazno navesti dobro adresu radi boljeg poziciniranja
+	$grad[0]="";//jako je vazno ako adresa ima dva imena povezati ih sa plus
+	$skracenica[0]=""; // AKo ne znate skracenicu ostavite praznu
+	$nazivlokacije[0]="In Cincinnati";
+	$telephone[0]=5132398810;// Broj koji se poziva na dugme CALL US
+
+	$adresa[1]="4265 San Felipe Suite 600 Houston TX 77027"; //jako je vazno navesti dobro adresu radi boljeg poziciniranja
+	$grad[1]="";//jako je vazno ako adresa ima dva imena povezati ih sa plus
+	$skracenica[1]=""; // AKo ne znate skracenicu ostavite praznu
+	$nazivlokacije[1]="In Houston";
+	$telephone[1]=7133555400;// Broj koji se poziva na dugme CALL US
+
+	
+
+	$companynumber=2;
+	$theme="b";//Thema koja se aplicira na sve
+	$transitionefect="turn"; //kucate tranziciju koju zelite na stranama . Vrste tranzicija : "fade", "flip", "pop", "slide", "slidedown", "slideup", "turn", "slidefade", "flow"
+?>
